0a65c6ed049636c466b1249c9d8f95c4114aa4fb
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-11-11 13:24:11 +0530

    Change type

--- sanity.js
+++ sanity.js
@@ -1,11 +1,11 @@
 import {createClient} from 'next-sanity';
 import createImageUrl from '@sanity/image-url';
 
-export const config:configType = {
+export const config = {
     dataset:process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion:"2021-03-25",
     useCdn:process.env.NODE_ENV == 'production'
 }
 export const sanityClient = createClient(config);
-export const urlFor = (source:any)=>createImageUrl(config).image(source);
+export const urlFor = (source)=>createImageUrl(config).image(source);

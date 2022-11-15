f517330b68defca7bc15caad707fb6febdd15ca9
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-11-15 23:16:03 +0530

    add

---
+++ utils/fetchSocial.ts
@@ -0,0 +1,8 @@
+import {Social} from '../typing';
+
+export const  fetchSocial = async ()=>{
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocial`);
+    const data = await res.json();
+    const social:Social[] = data.social;
+    return social;
+}

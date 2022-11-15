6850380ec86fc80dab879b59952564b0f7d3fda2
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-11-15 23:05:07 +0530

    add

--- pages/api/getProject.ts
+++ pages/api/getProject.ts
@@ -1,18 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'
 import {groq} from 'next-sanity';
 import {sanityClient} from '../../sanity';
-import {Experience} from '../../typing';
+import {Project} from '../../typing';
 const query = groq`
-*[_type=='experience']{
+*[_type=='project']{
   ...,
  technologies [] ->
 }
 `;
 interface DataType{
-    experience:Experience[]
+    project:Project[]
 }
 export default async function handler(req: NextApiRequest,res: NextApiResponse<DataType>) {
-  const experience:Experience[] = await sanityClient.fetch(query);
-  res.status(200).json({experience}); 
+  const project:Project[] = await sanityClient.fetch(query);
+  res.status(200).json({project}); 
 }
